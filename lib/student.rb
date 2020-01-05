4b9796df9fa76cf9110092f5eed46d8000f3c574
Ken Goad <goadke@g.cofc.edu>
2020-01-05 15:29:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
   def save
     sql = <<-SQL
     
-      INSERT INTO students(name, grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
       
       SELECT TOP 1 id,
